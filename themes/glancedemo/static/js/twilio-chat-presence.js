533c36c9986adff5e8d6b4d4ecd5f81470382537
Tom Kraft <tkraft@glance.net>
2020-10-14 15:25:14 -0400

    new twilio js file

--- themes/glancedemo/static/js/twilio-chat-presence.js
+++ themes/glancedemo/static/js/twilio-chat-presence.js
@@ -1,14 +1,14 @@
 jQuery.ready(() => {
   const appConfig = {
-    accountSid: “AC91f89e5d5e04366152c8e62572134fd7”,
-    flexFlowSid: “FO69eef6e849903fa7cfed2005524615ae”,
+    accountSid: "AC91f89e5d5e04366152c8e62572134fd7",
+    flexFlowSid: "FO69eef6e849903fa7cfed2005524615ae",
     context: {
-     friendlyName: “Jennifer Smith”,
+     friendlyName: "Jennifer Smith",
     }
    };
       Twilio.FlexWebChat.createWebChat(appConfig).then(webchat => {
        const { manager } = webchat;
-          manager.strings.WelcomeMessage = “Glance Twilio Flex Webchat”;
+          manager.strings.WelcomeMessage = "Glance Twilio Flex Webchat";
           webchat.init();
        // GLANCE PRESENCE CODE HERE:
          let visitor = new GLANCE.Presence.Visitor({
