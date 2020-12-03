0199557fb4ba1d5eecf3b3005367e39993348c31
Tom Kraft <tkraft@glance.net>
2020-12-03 10:57:53 -0500

    new js file

--- themes/glancedemo/static/js/twilio-chat-presence.js
+++ themes/glancedemo/static/js/twilio-chat-presence.js
@@ -2,22 +2,21 @@ jQuery(window).ready(()=> {
   const appConfig = {
     accountSid: "AC91f89e5d5e04366152c8e62572134fd7",
     flexFlowSid: "FO69eef6e849903fa7cfed2005524615ae",
-    context: {
-      friendlyName: document.URL.includes("accounts") ? "Jennifer Smith" : "Anonymous",
-    },
     startEngagementOnInit: true
   };
-  let firstMessage = true;
+
+  let sessionid = undefined;
+
   Twilio.FlexWebChat.createWebChat(appConfig)
     .then(webchat => {
       const { manager } = webchat;
+
       Twilio.FlexWebChat.Actions.on("afterSendMessage", () => {
-        if (firstMessage) {
-          const {channelSid} = manager.store.getState().flex.session;
+        const {channelSid} = manager.store.getState().flex.session;
+        if (!sessionid || sessionid !== channelSid) {
           manager
             .chatClient.getChannelBySid(channelSid)
             .then(channel => {
-              console.log(channel.sid);
               let visitor = new GLANCE.Presence.Visitor({
                 groupid: document.getElementById("glance-cobrowse").getAttribute("data-groupid"),
                 visitorid: channel.sid
@@ -26,19 +25,21 @@ jQuery(window).ready(()=> {
                 console.log("presence error:", e);
               };
               visitor.presence({
-                data: {mydata: "abc", myotherdata: 99999},
                 onsuccess: function () {
                   console.log("presence success");
                 }
               });
               visitor.onsignal = function (msg) {
-                console.log("received signal:", e);
+                console.log("received signal:", msg);
               };
               visitor.connect();
+
             });
-          firstMessage = false;
+          sessionid = channelSid;
         }
       });
+
       webchat.init();
+
     });
 });
