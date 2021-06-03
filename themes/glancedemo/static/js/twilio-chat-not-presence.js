ecf6fb73da3a7cb8f87ec051d1de17ba596393d3
ntosh <ntosh@glance.net>
2021-06-03 14:18:16 -0400

    testing

--- themes/glancedemo/static/js/twilio-chat-not-presence.js
+++ themes/glancedemo/static/js/twilio-chat-not-presence.js
@@ -1,7 +1,7 @@
 jQuery(window).ready(()=> {
           const appConfig = {
             accountSid: "ACed09bac96930c7f93887faa7d1be9136",
-            flexFlowSid: "FW6df8d491707ec52a12386b3e626e892e",
+            flexFlowSid: "FOdaee9774acf90f0d012a1acdc158b3aa",
            context: {
             friendlyName: /* is logged in ? then = Jennifer Smith, else = anonymous */
            },
